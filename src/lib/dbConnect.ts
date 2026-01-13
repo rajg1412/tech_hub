4152825aca1be98eb33ead776d3f9cca17e9f303
Raj Goyal <rajg50103@gmail.com>
2026-01-13 18:39:25 +0530

    Robustness fix for MONGODB_URI check in dbConnect

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
 let cached = (global as any).mongoose;
 
 if (!cached) {
@@ -9,6 +7,8 @@ if (!cached) {
 }
 
 async function dbConnect() {
+    const MONGODB_URI = process.env.MONGODB_URI;
+
     if (!MONGODB_URI) {
         throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
     }
